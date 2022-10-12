ade42173cbc45768389524effef4a04b721fccd6
Arvin <arvintian8@gmail.com>
2022-10-12 11:06:07 +0800

    upgrade go-utils version

--- {{cookiecutter.project}}/cmd/main.go
+++ {{cookiecutter.project}}/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/Arvintian/go-utils/cmdutil/flagtools"
-	"github.com/Arvintian/go-utils/cmdutil/pflagenv"
-	cli "github.com/rancher/wrangler-cli"
+	"fmt"
+
+	"github.com/Arvintian/go-utils/cmdutil"
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 )
@@ -11,21 +11,28 @@ import (
 var Version = "0.0.0-dev"
 
 type RootCommand struct {
-	Host string `name:"host" usage:"bind host" default:"0.0.0.0"`
-	Port int    `name:"port" usage:"bind port" default:"8000"`
+	Host    string `name:"host" usage:"bind host" default:"0.0.0.0"`
+	Port    int    `name:"port" usage:"bind port" default:"8000"`
+	Version bool   `name:"version" usage:"show version"`
 }
 
 func (r *RootCommand) Run(cmd *cobra.Command, args []string) error {
-	pflagenv.ParseSet(pflagenv.Prefix, cmd.Flags())
+	if r.Version {
+		return r.ShowVersion()
+	}
 	klog.Infof("Run on %s:%d", r.Host, r.Port)
 	<-cmd.Context().Done()
 	return nil
 }
 
+func (r *RootCommand) ShowVersion() error {
+	fmt.Println(Version)
+	return nil
+}
+
 func main() {
-	root := cli.Command(&RootCommand{}, cobra.Command{
+	root := cmdutil.Command(&RootCommand{}, cobra.Command{
 		Long: "{{cookiecutter.description}}",
 	})
-	flagtools.BindFlags(root.Flags())
-	cli.Main(root)
+	cmdutil.Main(root)
 }
--- {{cookiecutter.project}}/go.mod
+++ {{cookiecutter.project}}/go.mod
@@ -3,19 +3,13 @@ module {{cookiecutter.project}}
 go 1.18
 
 require (
-	github.com/Arvintian/go-utils v0.0.0-20221011104057-fcd81357086d
-	github.com/rancher/wrangler-cli v0.0.0-20220624114648-479c5692ba22
-	github.com/spf13/cobra v1.5.0
+	github.com/Arvintian/go-utils v0.0.0-20221012024917-654a94719d3c
+	github.com/spf13/cobra v1.6.0
 	k8s.io/klog/v2 v2.60.1
 )
 
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
-	github.com/rancher/wrangler v0.8.8 // indirect
-	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
-	k8s.io/klog v1.0.0 // indirect
 )
